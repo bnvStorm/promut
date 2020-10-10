82515cbd767099c0ffa41fed5c0120f132c1dda2
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-10-11 03:02:22 +0600

    вывод фото и цен в темлейте

--- template.php
+++ template.php
@@ -1,8 +1,9 @@
-<?php include 'header.php' ?>
-
+<?php include 'header.php';
+?>
+		
 		<h1 ><?php echo $h1; ?><!-- <br><div class="warning">Страница на обновлении</div> --></h1>
-		<img src="<?php echo $image; ?>" alt="">
-		<span class="price"><?php echo $price; ?></span>
+		<img src="/images/<?php echo $image; ?>" alt="" width='150px' height="150px" > <br>
+		<span class="price">Цена <?php echo $price; ?></span>
 		<div class='content_text'>
 			<?php echo $content; ?>
 		</div>	
