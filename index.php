af962e6e0a2873adb6135742ea10f2306e64c0e4
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-10-11 03:21:08 +0600

    create index file

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+
+require 'config.php';
+include 'header.php';
+?>
+
+<h2>Страницы</h2>
+<ul>
+<li> <a href="https://promutil.kz/g356425-kornevaya-gruppa">Ссылка</a></li>
+<li> <a href="https://promutil.kz/location_schema">Ссылка</a></li>
+<li> <a href="https://promutil.kz/product_list/page_2">Ссылка</a></li>
+<li> <a href="https://promutil.kz/testimonials">Ссылка</a></li>
+<li> <a href="https://promutil.kz/new_products">Ссылка</a></li>
+<li> <a href="https://promutil.kz/discounted_products">Ссылка</a></li>
+<li> <a href="https://promutil.kz/contacts">Ссылка</a></li>
+<li> <a href="https://promutil.kz/product_list">Ссылка</a></li>
+</ul>
+
+
+<h2>Товары</h2>
+<ul>
+<li> <a href="https://promutil.kz/p45999546-utilizatsiya-biologicheskih-othodov.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p51857535-drova-meshkah.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p48976745-drova.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p45999001-utilizatsiya-lyuminestsentnyh-lamp.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p51864968-poddon-derevyannyj.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p45999553-utilizatsiya-lekarstvennyh-preparatov.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p46000338-pererabotka-bytovoj-tehniki.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p46000343-razrabotka-pasportov-othodov.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p46000321-utilizatsiya-tary-upakovki.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p46000233-utilizatsiya-tamozhennyh-othodov.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p45668620-dokumentirovanie-othodov.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p45999559-utilizatsiya-kinoplenki-fiksazha.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p45684900-utilizatsiya-shin.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p51860994-etilenglikol-otrabotannyj.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p46000267-utilizatsiya-derevyannoj-tary.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p51860460-otrabotannoe-maslo-transformatornoe.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p45551525-kontejnernye-ploschadki.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p46000270-utilizatsiya-meditsinskih-biologicheskih.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p48976764-otrabotannoe-maslo.html">Ссылка</a></li>
+<li> <a href="https://promutil.kz/p45668577-vremennoe-hranenie-othodov.html">Ссылка</a></li>
+</ul>
+
+
+<?php
+
+include 'footer.php';
+?>
